4743cd47b4f17ca5acc0bab8df95ce24065da5f6
Mateus Pactiones <mateuspactiones@gmail.com>
2026-09-09 10:25:44 -0300

    Listas
    Add a list of AI models and print statements for demonstration.

---
+++ Aula2/listas.py
@@ -0,0 +1,6 @@
+Modelos_IA = ["YOLO", "SAM", "DINO", "CLIP", "LLaMA", "BERT", "T5", "ResNet", "VGG", "Inception",
+             "MobileNet", "EfficientNet", "Transformer", "RNN", "LSTM", "GRU"]
+
+print(Modelos_IA[0])
+print(Modelos_IA[1:4])
+print(Modelos_IA[:3])
