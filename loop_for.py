a179050fd6e57dfcba90f4721c1653ef7e41b10a
Mateus Pactiones <mateuspactiones@gmail.com>
2026-09-09 10:47:44 -0300

    Laços de repetição - for

---
+++ loop_for.py
@@ -0,0 +1,10 @@
+Treinamento_IA = {
+  "Integrantes": ["Evandro", "Luigi", "Evelyn", "Marcelo", "Pedro", "Gustavo", "Mateus", "Felipe"],
+  "Cargos": ["Professor", "Estudante"]
+}
+
+for integrante in Treinamento_IA["Integrantes"]:
+  print(integrante)
+
+for cargo in Treinamento_IA["Cargos"]:
+  print(cargo)
