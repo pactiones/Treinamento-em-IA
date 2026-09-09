78df38fbd5ead13f45a4befe0402576504fffe00
Mateus Pactiones <mateuspactiones@gmail.com>
2026-09-09 10:34:32 -0300

    Dicionario

---
+++ Aula2/Dicionario.py
@@ -0,0 +1,9 @@
+treinamento_IA = {
+  "Integrantes": ["Evandro", "Luigi", "Evelyn", "Marcelo", "Pedro", "Gustavo", "Mateus", "Felipe"],
+  "Cargos": ["Professor, estudante"]
+}
+
+print(treinamento_IA["Integrantes"])
+print(treinamento_IA["Cargos"])
+print(treinamento_IA["Integrantes"][0])
+print(treinamento_IA["Cargos"][0])
